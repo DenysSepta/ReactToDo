78fd64f19beb45131f482b307f24deefcbe29698
Denys Septa <septaden@gmail.com>
2019-11-30 08:16:02 +0100

    gh-pages update

---
+++ build/precache-manifest.c868f7b72cf8fa0058c3746924cc027c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce86881b69d55a2f930e2a77094f08e",
+    "url": "/rectToDo/index.html"
+  },
+  {
+    "revision": "908d40158bb6f40222fd",
+    "url": "/rectToDo/static/css/main.fe9f76cf.chunk.css"
+  },
+  {
+    "revision": "29ba6a5161fc628e56be",
+    "url": "/rectToDo/static/js/2.8cef289f.chunk.js"
+  },
+  {
+    "revision": "2a6c8ee3b85982a73f6c",
+    "url": "/rectToDo/static/js/3.2d821667.chunk.js"
+  },
+  {
+    "revision": "908d40158bb6f40222fd",
+    "url": "/rectToDo/static/js/main.de30eaed.chunk.js"
+  },
+  {
+    "revision": "45b8eb2f8b9d451e2272",
+    "url": "/rectToDo/static/js/runtime~main.323cd2f3.js"
+  }
+]);
